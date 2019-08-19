7f7d6e87d5cbddab19a80ac53e481ba7fca58f3c
xraybat <xraybat100@gmail.com>
2019-08-19 15:00:12 +1000

    clock code

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -8,6 +8,7 @@ import scala.scalajs.js.annotation.JSExport
 import org.scalajs.dom
 import org.scalajs.dom.html
 import scala.util.Random
+import scalajs.js
 
 case class Point(x: Int, y: Int){
   def +(p: Point) = Point(x + p.x, y + p.y)
@@ -27,24 +28,30 @@ object ScalaJSExample {
     canvas.width = canvas.parentElement.clientWidth
     canvas.height = canvas.parentElement.clientHeight
 
-    renderer.fillStyle = "#f8f8f8"
-    renderer.fillRect(0, 0, canvas.width, canvas.height)
+    val gradient = renderer.createLinearGradient(
+      canvas.width / 2 - 100, 0, canvas.width / 2 + 100, 0)
+    gradient.addColorStop(0, "red")
+    gradient.addColorStop(0.5, "green")
+    gradient.addColorStop(1, "blue")
+    renderer.fillStyle = gradient
+    renderer.textAlign = "center"
+    renderer.textBaseline = "middle"
 
     // do it 
-    renderer.fillStyle = "black"
-    var down = false
-    
-    canvas.onmousedown = (e: dom.MouseEvent) => down = true
-    canvas.onmouseup = (e: dom.MouseEvent) => down = false
+    def format(n: Int) = f"$n%02d"
 
-    canvas.onmousemove = {
-      (e: dom.MouseEvent) =>
-        val rect = canvas.getBoundingClientRect()
-        if (down)
-          renderer.fillRect(e.clientX - rect.left,
-                            e.clientY - rect.top,
-                            10, 10)
+    def render() = {
+      val date = new js.Date()
+      renderer.clearRect(0, 0, canvas.width, canvas.height)
+      renderer.font = "75px sans-serif"
+      renderer.fillText(Seq(date.getHours(),
+                            date.getMinutes(),
+                            date.getSeconds()).map(format).mkString(":"),
+                        canvas.width / 2,
+                        canvas.height / 2)
     }
+
+    dom.window.setInterval(() => render, 1000)
 
   } // main
 } // ScalaJSExample

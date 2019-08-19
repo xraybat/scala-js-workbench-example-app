b8386d0ffda2d443ec56638601ec2b55079fe11a
xraybat <xraybat100@gmail.com>
2019-08-19 14:42:33 +1000

    sketchpad code

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -20,37 +20,31 @@ object ScalaJSExample {
   def main(canvas: html.Canvas): Unit = {
     println("main...")
 
-    val ctx = canvas.getContext("2d")
-                    .asInstanceOf[dom.CanvasRenderingContext2D]
+    // setup
+    val renderer = canvas.getContext("2d")
+                     .asInstanceOf[dom.CanvasRenderingContext2D]
 
-    var count = 0
-    var p = Point(0, 0)
-    val corners = Seq(Point(255, 255),
-                      Point(0, 255),
-                      Point(128, 0))
+    canvas.width = canvas.parentElement.clientWidth
+    canvas.height = canvas.parentElement.clientHeight
 
-    def clear() = {
-      ctx.fillStyle = "white"
-      ctx.fillRect(0, 0, 255, 255)
+    renderer.fillStyle = "#f8f8f8"
+    renderer.fillRect(0, 0, canvas.width, canvas.height)
+
+    // do it 
+    renderer.fillStyle = "black"
+    var down = false
+    
+    canvas.onmousedown = (e: dom.MouseEvent) => down = true
+    canvas.onmouseup = (e: dom.MouseEvent) => down = false
+
+    canvas.onmousemove = {
+      (e: dom.MouseEvent) =>
+        val rect = canvas.getBoundingClientRect()
+        if (down)
+          renderer.fillRect(e.clientX - rect.left,
+                            e.clientY - rect.top,
+                            10, 10)
     }
-
-    def run = for (i <- 0 until 10) {
-      if (count % 3000 == 0)
-        clear()
-      count += 1
-      p = (p + corners(Random.nextInt(3))) / 2
-
-      val height = 512.0 / (255 + p.y)
-      val r = (p.x * height).toInt
-      val g = ((255 - p.x) * height).toInt
-      val b = p.y
-
-      ctx.fillStyle = s"rgb($g, $r, $b)"
-      ctx.fillRect(p.x, p.y, 1, 1)
-
-    } // run
-
-    dom.window.setInterval(() => run, 50)
 
   } // main
 } // ScalaJSExample
